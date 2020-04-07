3177761476047e24120deb961062b9cbec656f56
Tiago Yamamoto <git@rede5.com.br>
2020-04-07 17:18:48 -0300

    Crud funcionários

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Funcionario
 
 
 def home(request):
     return HttpResponse('Ola')
+
+
+class FuncionariosList(ListView):
+    model = Funcionario
