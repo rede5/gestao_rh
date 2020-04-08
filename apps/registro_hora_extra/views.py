b987944e799e9e0ae568f79ef971eb03c5dd70a5
Tiago Yamamoto <git@rede5.com.br>
2020-04-08 15:52:13 -0300

    Crud Update Registro de Hora Extra

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 
 from .models import RegistroHoraExtra
 from django.shortcuts import render
@@ -10,4 +10,9 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
 
