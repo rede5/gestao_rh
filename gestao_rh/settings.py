40e7a3b46d8fa1b0f8632ba4ccd0b04f7f836df5
Tiago Yamamoto <git@rede5.com.br>
2020-04-08 02:14:46 -0300

    Crud de Documentos

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -86,11 +86,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "staticfiles")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = 'home'
 
