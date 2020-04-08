f0daec46d56b173ccdc8858366b3d79352a02206
Tiago Yamamoto <git@rede5.com.br>
2020-04-08 15:24:57 -0300

    Listagem do banco de horas

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,13 @@
+from django.views.generic import ListView
+
+from .models import RegistroHoraExtra
 from django.shortcuts import render
 
-# Create your views here.
+
+class HoraExtraList(ListView):
+    model = RegistroHoraExtra
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
